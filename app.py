b75de5f83e11e2c3eb8475101e1dd48f00a60e9a
archanjojr <math.calazans56@gmail.com>
2017-10-30 10:00:35 -0200

    add travis ci file

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ import pprint
 import json
 import sys
 
-list = [ 'en-US', 'de-DE', 'es-ES', 'es-MX', 'fr-FR', 'it-IT', 'ja-JP', 'pl-PL', 'pt-BR', 'ru-RU']
-lang=0
-if len(sys.argv)>0:
-    if sys.argv[1] in list:
-        lang=list.index(sys.argv[1])
-print("Getting a JSON for the "+ list[lang]+" language")
+langs = ['en-US', 'de-DE', 'es-ES', 'es-MX', 'fr-FR', 'it-IT', 'ja-JP', 'pl-PL', 'pt-BR', 'ru-RU']
+index = 0
+if len(sys.argv) > 0:
+    if sys.argv[1] in langs:
+        index = langs.index(sys.argv[1])
+print("Getting a JSON for the " + langs[index] + " indexuage")
 
-'''
 url = 'https://api.gwentapi.com/v0/cards'
 print('Getting the Amount of Cards of Database')
 pag = re.get(url)
@@ -20,28 +19,25 @@ if pag.status_code != 200:
 else:
     limit = pag.json()['count']
 
-url += "?limit=" + str(limit)+'&lang='+list[lang]
-
+url += "?limit=" + str(limit) + '&index=' + langs[index]
 
 pag = re.get(url)
 if pag.status_code != 200:
     print(pag.status_code)
 else:
-    with open('aux.json','w') as arquivo:
+    with open('aux.json', 'w') as arquivo:
         arquivo.write(json.dumps(pag.json()["results"]))
 
-test = limit
+size = limit
 aux = json.load(open('aux.json', 'r'))
 file = open('cards.json', 'w')
-file.write('[')
-file.close()
-
-for i in range(test):
-    print("Making the request for : "+aux[i]['name'])
+file.write('['), file.close()
+for i in range(size):
+    print("Making the request for : " + aux[i]['name'])
     cart = re.get(aux[i]['href'])
     with open("cards.json", 'a')as file:
-        if i < (test-1):
-            print("Writing the request for : "+aux[i]['name']+" in file.")
+        if i < (size - 1):
+            print("Writing the request for : " + aux[i]['name'] + " in file.")
             file.write(json.dumps(cart.json()) + ",")
         else:
             print("Writing the request for : " + aux[i]['name'] + " in file.")
@@ -49,22 +45,19 @@ for i in range(test):
 
 cartas = json.load(open('cards.json', 'r'))
 file = open('variations.json', 'w')
-file.write('[')
-file.close()
-test = limit
-for i in range(test):
-    print('Getting Variations for: '+cartas[i]['name'])
+file.write('['), file.close()
+for i in range(size):
+    print('Getting Variations for: ' + cartas[i]['name'])
     var = re.get(cartas[i]['variations'][0]['href'])
     with open('variations.json', 'a') as file:
-        if i < (test-1):
-            print('writing variations for :'+cartas[i]['name'])
-            file.write(json.dumps(var.json())+',')
+        if i < (size - 1):
+            print('writing variations for :' + cartas[i]['name'])
+            file.write(json.dumps(var.json()) + ',')
         else:
-            print('writing variations for :'+cartas[i]['name'])
+            print('writing variations for :' + cartas[i]['name'])
             file.write(json.dumps(var.json()) + ']')
 print('CLEANING THE THE JSONS')
 clean_cards('cards.json')
 clean_variations('variations.json')
 delete('aux.json')
 print('Done!')
-'''
